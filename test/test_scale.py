b6565563813c9109a1a493e7236124d58da55c1d
userid392 <>
2020-06-06 14:36:18 +0900

    update test

--- test/test_scale.py
+++ test/test_scale.py
@@ -7,19 +7,18 @@ from dlchord.const import SCALE
 
 class TestScale(unittest.TestCase):
     def test_length(self):
-        for k, scale in SCALE.keys():
-            self.assertEqual(len(scale), 12)
+        for k, scale in SCALE.items():
+            self.assertEqual(len(scale), 12, "Error: {}".format(k))
 
     def test_duplicates(self):
-        for k, scale in SCALE.keys():
-            self.assertTrue(len(scale) != len(set(scale)))
+        for k, scale in SCALE.items():
+            self.assertTrue(len(scale) == len(set(scale)), "Error: {}".format(k))
 
     def test_notes(self):
         notes = list(range(12))
-        for k, scale in SCALE.keys():
+        for k, scale in SCALE.items():
             scale = sorted([Chord(note).bass for note in scale])
-
-            self.assertEqual(notes, scale)
+            self.assertEqual(notes, scale, "Error: {}".format(k))
 
 
 if __name__ == '__main__':
